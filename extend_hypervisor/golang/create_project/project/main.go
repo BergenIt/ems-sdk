da58a1cef3646a1876870da477f721c8fa06dc6b
Герман Генералов <g.generalov@gagarin.me>
2024-03-05 13:57:48 +0300

    #30685 add hypervisor example

--- extend_hypervisor/golang/create_project/project/main.go
+++ extend_hypervisor/golang/create_project/project/main.go
@@ -7,6 +7,7 @@ import (
 	pb "hypervisor/gen/cluster-contract"
 	"log"
 	"net"
+	"os"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -23,25 +24,41 @@ func main() {
 }
 
 func run() error {
+	port := os.Getenv("ServicePort")
+	if port == "" {
+		port = listenPort
+	}
+
+	// Создаем инстанс сервиса.
 	m := microservice{}
+
+	// Создаем инстанс сервера.
 	server := grpc.NewServer()
 
+	// Регистрируем сервис.
 	pb.RegisterHypervisorManagerServer(server, &m)
+
+	// Регистрируем рефлексию для сервиса, чтобы получать информацию об общедоступных RPC (опционально).
 	reflection.Register(server)
 
+	// Создаем листененра.
 	lis, err := net.Listen("tcp", listenPort)
 	if err != nil {
 		return fmt.Errorf("create listener: %s", err)
 	}
 
-	log.Printf("microservice start serving on port %q", listenPort)
+	log.Printf("microservice start serving on port %q", port)
+
+	// Запускаем gRPC сервер.
 	return server.Serve(lis)
 }
 
+// Инстанс сервиса с реализацией RPC.
 type microservice struct {
 	pb.UnimplementedHypervisorManagerServer
 }
 
+// RPC по сбору списка виртуальных машин с гипервизра ESXI.
 func (r *microservice) CollectVirtialMachinesList(context.Context, *pb.CollectVirtialMachinesListRequest) (*pb.CollectVirtialMachinesListResponse, error) {
 	//реализация rpc
 	//...
