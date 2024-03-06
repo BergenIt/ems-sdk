fb37aadbc005b7078f7406e2f65400c85feea837
Максим Рекуданов <maksim.rekudanov@bergen.tech>
2024-03-06 21:36:16 +0300

    #30682 change project

--- extend_bmc/dotnet/create_project/project/Program.cs
+++ extend_bmc/dotnet/create_project/project/Program.cs
@@ -1,13 +1,17 @@
-using BmcManager.Services;
+using BmcHandler.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+// Add gRPC reflection to the container.
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<MyService>();
+app.MapGrpcService<BmcHandlerService>();
+// Map incoming requests to the gRPC reflection service.
+app.MapGrpcReflectionService();
 
-app.Run();
+await app.RunAsync();
---
+++ extend_bmc/dotnet/create_project/project/Services/BmcHandler.cs
@@ -0,0 +1,19 @@
+﻿using Grpc.Core;
+
+using ToolCluster.V4;
+
+namespace BmcHandler.Services
+{
+    public class BmcHandlerService : ToolCluster.V4.BmcManager.BmcManagerBase
+    {
+        public override async Task<CollectBmcLedStateResponse> CollectLedState(CollectBmcLedStateRequest request, ServerCallContext context)
+        {
+            throw new RpcException(new Status(StatusCode.Unimplemented, ""));
+        }
+
+        public override async Task<BmcFirmwareUpdateResponse> BmcFirmwareUpdate(BmcFirmwareUpdateRequest request, ServerCallContext context)
+        {
+            throw new RpcException(new Status(StatusCode.Unimplemented, ""));
+        }
+    }
+}
