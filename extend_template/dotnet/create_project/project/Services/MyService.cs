5475f1a29b6178bf80652cdfe6bd20ece509a846
Егор Головин <e.golovin@gagarin.me>
2024-03-06 11:40:13 +0300

    @skip

---
+++ extend_template/dotnet/create_project/project/Services/MyService.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Core;
+
+namespace SnmpExample.Services
+{
+    public class MyService : MyProtoService.MyProtoServiceBase
+    {
+        public override Task<PingReply> SendPing(PingRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new PingReply { RespondString = "Respond string"});
+        }
+    }
+}
